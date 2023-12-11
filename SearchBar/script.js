743cfdc56eab8b11f4e6868f30122d012fe36e64
SeniorCub <farinderif@gmail.com>
2023-12-11 16:16:45 +0200

    ........

--- SearchBar/script.js
+++ SearchBar/script.js
@@ -1,13 +1,14 @@
 // api key = 96ee6e6e97ca47e09c33c780cc713cf5
 document.addEventListener('DOMContentLoaded', function () {
      const apiKey = '96ee6e6e97ca47e09c33c780cc713cf5'; // Replace with your News API key
-     const apiUrl = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${apiKey}`;
+     const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`;
  
      // Fetch news articles from News API
      fetch(apiUrl)
          .then(response => response.json())
          .then(data => {
              if (data.status === 'ok') {
+               console.log(data);
                  displayNews(data.articles);
              } else {
                  console.error('Error fetching news:', data.message);
@@ -26,9 +27,12 @@ document.addEventListener('DOMContentLoaded', function () {
              articleElement.className = 'news-article';
  
              articleElement.innerHTML = `
-                 <div class="news-title">${article.title}</div>
-                 <div class="news-description">${article.description}</div>
-                 <a href="${article.url}" target="_blank">Read more</a>
+               <a href="${article.url}" target="_blank">
+                    <div class="news-image"><img src="${article.urlToImage
+                    }" ></div>
+                    <div class="news-title">${article.title}</div>
+                    <div class="news-description">${article.description}</div>
+               </a>
              `;
  
              newsContainer.appendChild(articleElement);

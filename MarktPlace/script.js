67a87989f70460864655b127fff7fca56594d57a
SeniorCub <farinderif@gmail.com>
2024-01-04 13:14:03 +0100

    Update script.js

--- MarktPlace/script.js
+++ MarktPlace/script.js
@@ -51,7 +51,7 @@ document.querySelector(".plus").addEventListener("click",()=>{
 document.querySelector(".car").addEventListener("click",()=>{
      let cartmenu = document.querySelector(".carts")
      
-     //  Finally, a ternary operator is used to toggle the display property of the "carts" element. If extended is true (i.e., the element is currently visible), the display property is set to "none", which hides the element. If extended is false (i.e., the element is currently hidden), the display property is set to "block", which makes the element visible.
+     // //  Finally, a ternary operator is used to toggle the display property of the "carts" element. If extended is true (i.e., the element is currently visible), the display property is set to "none", which hides the element. If extended is false (i.e., the element is currently hidden), the display property is set to "block", which makes the element visible.
 
      let extended = cartmenu.style.display === "block"
      cartmenu.style.display = extended? "none" : "block"

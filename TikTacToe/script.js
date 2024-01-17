da65811e2e19241014fa04a7dc1690a614c5d423
SeniorCub <farinderif@gmail.com>
2024-01-17 13:52:21 +0100

    Update script.js

--- TikTacToe/script.js
+++ TikTacToe/script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+     let xx = document.querySelector(".xx");
+     let oo = document.querySelector(".oo");
+     let pickx = document.querySelector(".pickx");
+     let picko = document.querySelector(".picko");
+     document.querySelector("#c1").addEventListener('click', () => {
+               if (pickx.checked == true) {
+                    xx.style.display = "block";
+               } else if (picko.checked == true) {
+                    oo.style.display = "block";
+               } else {
+                    alert("Please select a player");
+               }
+          });
 });

7c32b4095847c092644b9e1acaf39a0c9bc06e56
seniorcub <farinderif@gmail.com>
2024-02-14 13:27:40 +0100

    Refactor CSS styles and update HTML structure

--- MarktPlace/script.js
+++ MarktPlace/script.js
@@ -1,116 +1,4 @@
-// fetch('https://fakestoreapi.com/products?limit=12')
-// .then (data => data.json())
-// .then(data2 => data2.forEach(element => {
-// document.querySelector('.cards').innerHTML += `
-
-
-//      <div class="p-2 col-lg-2 col-md-5 col-10 cardd">
-//           <div class="img">
-//                <img src="${element.image}" alt="....">
-//           </div>
-//           <div class="tag">${element.category}</div>
-//           <div class="name">
-//                ${element.title} 
-//           </div>
-//           <div class="d-flex justify-content-between price">
-//                <div class="new">$${element.price}</div>
-//                (${element.rating.count})
-//           </div>
-//           <div class="rate">
-//                <i class="bi bi-star-fill"></i>
-//                <i class="bi bi-star-fill"></i>
-//                <i class="bi bi-star-fill"></i>
-//                <i class="bi bi-star-half"></i>
-//                <i class="bi bi-star"></i> <span class="dis">${element.rating.rate}</span>
-//           </div>
-//           <div class="deli">
-//                Jumia <span class="exp"><i class="bi bi-send-fill"></i>Express</span>
-//           </div>
-//           <div class="btn btnn addcart">ADD TO CART</div>
-//           <div class="ggg">
-//                <div class="d-flex btnnn addition">
-//                     <div class="btn btnn bt minus">-</div>
-//                     <span class="dig">1</span>
-//                     <div class="btn btnn bt plus">+</div>
-//                </div>
-//           </div>
-//      </div>
-// `
-
-// }));
-
-
-document.querySelectorAll(".addcart").forEach((element)=>{
-     element.style.display = "none"
-})
-document.querySelectorAll(".ggg").forEach((element)=>{
-     element.style.display = "none"
-})
 document.querySelector(".carts").style.display = "none";
-function adddd() {
-     let badge = document.querySelector(".badge")
-     let dig = document.querySelector(".dig")
-     let nnn = document.querySelector(".nnn")
-     num = Number(badge.innerText) + 1
-     badge.innerText = num
-     dig.innerText = num
-     nnn.innerText = num
-     if (num > 0) {
-          document.querySelector(".card").removeEventListener
-     }
-}
-function reduce() {
-     let badge = document.querySelector(".badge")
-     let dig = document.querySelector(".dig")
-     let nnn = document.querySelector(".nnn")
-     num = Number(badge.innerText) - 1;
-     if (num <= 0) {
-          let btn = document.querySelector(".ggg")
-          btn.style.display = "none"
-     }
-     badge.innerText = num
-     dig.innerText = num
-     nnn.innerText = num
-}
-document.querySelectorAll(".card").forEach((e)=>{
-     e.addEventListener("mouseover",()=>{
-          let btnn = document.querySelectorAll(".addcart")
-           btnn.forEach((e)=>{
-               e.style.display = "block"
-           })
-     })
-     e.addEventListener("mouseout", () => {
-          let btnn = document.querySelectorAll(".addcart")
-          btnn.forEach((e)=>{
-               e.style.display = "none"
-          })
-     })}
-)
-document.querySelectorAll(".addcart").forEach((e)=>{
-     e.addEventListener("click",()=>{
-          let btnn = document.querySelectorAll(".addcart")
-          btnn.forEach((e)=>{
-               e.style.display = "none"
-          })
-          let btn = document.querySelectorAll(".ggg")
-          btn.forEach((e)=>{
-               e.style.display = "block"
-          })
-          adddd()
-          cart()
-     })
-})
-document.querySelectorAll(".minus").forEach((e)=>{
-     e.addEventListener("click",()=>{
-          reduce()
-     })
-})
-document.querySelectorAll(".plus").forEach((e)=>{
-     e.addEventListener("click",()=>{
-          adddd()
-     })
-})
-
 // For the Cart
 document.querySelector(".car").addEventListener("click",()=>{
      let cartmenu = document.querySelector(".carts")
@@ -120,43 +8,112 @@ document.querySelector(".car").addEventListener("click",()=>{
      let extended = cartmenu.style.display === "block"
      cartmenu.style.display = extended? "none" : "block"
 })
-function cart() {
-     document.querySelector(".goods").innerHTML = `
-                         <div class="good">
-                              <div class="goo">
-                                   <div class="img">
-                                        <img src="1.jpg" alt="....">
-                                  </div>
-     
-                                  <div class="tavvv">
-                                        <div class="name">
-                                             XIAOMI Redmi A2 + 6.52*2GB RAM/32GB ROM Android 12 - Black
-                                        </div>
-                                        <div class="expp">Few units left</div>
-                                        <div class="deli">
-                                             Jumia <span class="exp"><i class="bi bi-send-fill"></i>Express</span>
-                                       </div>
-                                  </div>
-     
-                                  <div class="price">
-                                        <div class="new">#61,800</div>
-                                        <div class="">
-                                             <span class="old"> #71,000</span>
-                                             <span class="dis">-13%</span>
-                                        </div>
-                                  </div>
-                              </div>
 
-                              <div class="gpoo">
-                                   <div class="btn btttt"><i class="bi bi-basket3-fill"></i>  REMOVE</div>
-                                   <div class="ggg">
-                                         <div class="d-flex btnnn addition">
-                                              <div class="btn btnn bt minus">-</div>
-                                              <span class="dig">1</span>
-                                              <div class="btn btnn bt plus">+</div>
-                                         </div>
-                                   </div>
-                              </div>
-                         </div>
-                         `
+
+let list = document.querySelector(".list");
+let listCard = document.querySelector(".listcard");
+let body = document.querySelector("body");
+let total = document.querySelector(".total");
+let quantity = document.querySelector(".quantity");
+
+
+let products = [
+     {
+          id: 1,
+          name: 'PRODUCT 1',
+          image: 'Images/One.jpeg',
+          price: 120000
+     },
+     {
+          id: 2,
+          name: 'PRODUCT 2',
+          image: 'Images/Two.jpeg',
+          price: 100000
+     },
+     {
+          id: 3,
+          name: 'PRODUCT 3',
+          image: 'Images/Three.jpeg',
+          price: 150000
+     },
+     {
+          id: 4,
+          name: 'PRODUCT 4',
+          image: 'Images/Three.jpeg',
+          price: 70000
+     },
+     {
+          id: 5,
+          name: 'PRODUCT 5',
+          image: 'Images/One.jpeg',
+          price: 200000
+     },
+     {
+          id: 6,
+          name: 'PRODUCT 6',
+          image: 'Images/Two.jpeg',
+          price: 250000
+     }
+];
+
+let listCards =[];
+
+function initApp () {
+     products.forEach((value, key) => {
+          let newDiv = document.createElement('div');
+          newDiv.classList.add('item');
+          newDiv.innerHTML = `
+          <img src="${value.image}" alt="product" width="100px">
+          <div class="title">${value.name}</div>
+          <div class="price">${value.price.toLocaleString()}</div>
+          <button onclick="addToCard(event, ${key})">Add To Cart</button>
+          `;
+          list.appendChild(newDiv);
+     })
+}
+initApp();
+
+function addToCard(event, key) {
+     event.preventDefault();
+     if (listCards[key] == null) {
+          listCards[key] = {...products[key], quantity: 1};
+     }
+     reloadCard();
+}
+
+function reloadCard() {
+     listCard.innerHTML = ``;
+     let count = 0;
+     let totalPrice = 0;
+     listCards.forEach((value, key) => {
+          totalPrice = totalPrice + value.price;
+          count = count + value.quantity;
+
+          if (value != null) {
+          let newDiv = document.createElement('li');
+          newDiv.innerHTML = `
+               <div><img src="${value.image}" alt="" width=""></div>
+               <div>${value.name}</div>
+               <div>${value.price.toLocaleString()}</div>
+               <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+               </div>
+          `;
+          listCard.appendChild(newDiv);
+          }
+     })
+     total.innerText = totalPrice.toLocaleString();
+     quantity.innerText = count;
+}
+
+function changeQuantity(key, quantity) {
+     if (quantity == 0) {
+          delete listCards[key];
+     }else {
+          listCards[key].quantity = quantity;
+          listCards[key].price = quantity * products[key].price;
+     }
+     reloadCard();
 }
